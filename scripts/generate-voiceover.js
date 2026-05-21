79c5e81b51ed22e624c97ed20df7593a04d03c86
pinflow1 <eonu4112@gmail.com>
2026-05-21 12:34:09 +0100

    Create generate-voiceover.js

---
+++ scripts/generate-voiceover.js
@@ -0,0 +1,25 @@
+const { exec } = require('child_process');
+const util = require('util');
+const fs = require('fs');
+const path = require('path');
+
+const execPromise = util.promisify(exec);
+
+async function main() {
+  const inputFile = process.argv.find(arg => arg === '--input') 
+    ? process.argv[process.argv.indexOf('--input') + 1] 
+    : 'output/script.txt';
+  const outputFile = process.argv.find(arg => arg === '--output')
+    ? process.argv[process.argv.indexOf('--output') + 1]
+    : 'output/voiceover.mp3';
+
+  const script = fs.readFileSync(inputFile, 'utf-8');
+  const voice = 'en-US-JennyNeural';
+  const escaped = script.replace(/"/g, '\\"').replace(/\n/g, ' ');
+  const cmd = `npx edge-tts --voice "${voice}" --text "${escaped}" --write-media ${outputFile}`;
+
+  await execPromise(cmd);
+  console.log(`✅ Voiceover saved to ${outputFile}`);
+}
+
+main().catch(err => { console.error(err); process.exit(1); });
