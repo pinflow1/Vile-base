98da3d63db85d592ad8b4388f6ccab3275d8920f
pinflow1 <eonu4112@gmail.com>
2026-05-22 10:26:30 +0100

    Update Root.tsx

--- src/Root.tsx
+++ src/Root.tsx
@@ -4,19 +4,16 @@ import { VileComposition } from './VileComposition';
 
 export const RemotionRoot: React.FC = () => {
   return (
-    <>
-      <Composition
-        id="VileComposition"
-        component={VileComposition}
-        durationInFrames={900}
-        fps={30}
-        width={1080}
-        height={1920}
-        defaultProps={{
-          scriptText: 'Vile analyzes your diffs and predicts runtime failures.',
-          audioUrl: '',
-        }}
-      />
-    </>
+    <Composition
+      id="VileComposition"
+      component={VileComposition}
+      durationInFrames={900}
+      fps={30}
+      width={1080}
+      height={1920}
+      defaultProps={{
+        scriptText: 'Vile analyzes your diffs and predicts runtime failures.',
+      }}
+    />
   );
 };
