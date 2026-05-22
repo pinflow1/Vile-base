7867e830ea95a3b9269c8ac0c26ddf6c4128e814
pinflow1 <eonu4112@gmail.com>
2026-05-22 19:04:29 +0100

    Update Root.tsx

--- src/Root.tsx
+++ src/Root.tsx
@@ -7,11 +7,11 @@ export const RemotionRoot: React.FC = () => {
     <Composition
       id="VileComposition"
       component={VileComposition}
-      durationInFrames={900}
+      durationInFrames={300} // Adjust based on total scene duration
       fps={30}
       width={1080}
       height={1920}
-      defaultProps={{ scriptText: 'Vile analyzes your diffs. Predicts runtime failures. Try Vile today.' }}
+      defaultProps={{}}
     />
   );
 };
